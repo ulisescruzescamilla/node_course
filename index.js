41b1cb801d2ce8bb9e6e9e3024cdc1155e519e64
Ulises Escamilla <desarrollo3@orbeseguros.com.mx>
2023-02-18 06:42:44 -0600

    Add cors

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
 
 app.use(express.json()); // middleware cast request to JSON
-
+const whitelist = ['http://localhost'];
+const options = (req, callback) => {
+  let corsOptions = {origin: false};
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = {origin: true};
+  }
+  return callback(null, corsOptions)
+}
+app.use(cors(options));
 routerApi(app);
 
 app.use(logErrors);
